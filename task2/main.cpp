4e186182118a3d01bf2586a2ca9133003915d774
odheL42 <99417770+odheL42@users.noreply.github.com>
2023-03-07 20:52:42 +0700

    Add files via upload

--- task2/main.cpp
+++ task2/main.cpp
@@ -25,35 +25,35 @@ int main(int argc, char** argv) {
     clock_t start, end; 
     int full_size = grid_size * grid_size;
     double step = (20 - 10) / (grid_size - 1);
-    auto* kern_1 = new double[full_size];
-    auto* kern_2 = new double[full_size];
-    std::memset(kern_1, 0, sizeof(double) * full_size);
+    auto* A = new double[full_size];
+    auto* Anew = new double[full_size];
+    std::memset(A, 0, sizeof(double) * full_size);
 
     //инициализируем углы
-    kern_1[0] = 10;
-    kern_1[grid_size - 1] = 20;
-    kern_1[full_size - 1] = 30;
-    kern_1[grid_size * (grid_size - 1)] = 20;
+    A[0] = 10;
+    A[grid_size - 1] = 20;
+    A[full_size - 1] = 30;
+    A[grid_size * (grid_size - 1)] = 20;
 
     start = clock();
-#pragma acc enter data copyin(kern_1[0:full_size]) create(kern_2[0:full_size])
+#pragma acc enter data copyin(A[0:full_size]) create(Anew[0:full_size])
     {
 #pragma acc parallel loop seq gang num_gangs(size) vector vector_length(size)
         //заполняем(рассчитываем) рамку матрицы
         for (int i = 1; i < grid_size - 1; i++) {
-            kern_1[i] = 10 + i * step;
-            kern_1[i * (grid_size - 1)] = 20 + i * step;
-            kern_1[i * grid_size] = 10 + i * step;
-            kern_1[grid_size * (grid_size - 1) + i] = 20 + i * step;
+            A[i] = 10 + i * step;
+            A[i * (grid_size - 1)] = 20 + i * step;
+            A[i * grid_size] = 10 + i * step;
+            A[grid_size * (grid_size - 1) + i] = 20 + i * step;
         }
     }
-    std::memcpy(kern_2, kern_1, sizeof(double) * full_size);
+    std::memcpy(Anew, A, sizeof(double) * full_size);
 
 
     double error = 1.0, min_error = accuracy;
     int max_iter = iters, iter = 0;
 
-#pragma acc enter data copyin(kern_2[0:full_size], kern_1[0:full_size], error)
+#pragma acc enter data copyin(Anew[0:full_size], A[0:full_size], error)
     
     while (error > min_error && iter < max_iter) {
         iter++;
@@ -63,18 +63,18 @@ int main(int argc, char** argv) {
 #pragma acc update device(error) async(1)
         }
 
-#pragma acc data present(kern_1, kern_2, error)
+#pragma acc data present(A, Anew, error)
 #pragma acc parallel loop independent collapse(2) vector vector_length(256) gang num_gangs(256) reduction(max:error) async(1)
         
         for (int i = 1; i < grid_size - 1; i++)
         {//выполняем основные вычисления - jacobi iteration
             for (int j = 1; j < grid_size - 1; j++)
             {
-                kern_2[i * grid_size + j] = 0.25 * (kern_1[i * grid_size + j - 1]
-                                            + kern_1[(i - 1) * grid_size + j] 
-                                            + kern_1[(i + 1) * grid_size + j] 
-                                            + kern_1[i * grid_size + j + 1]);
-                error = fmax(error, kern_2[i * grid_size + j] - kern_1[i * grid_size + j]); //пересчитываем(узнаём) ошибку
+                Anew[i * grid_size + j] = 0.25 * (A[i * grid_size + j - 1]
+                                            + A[(i - 1) * grid_size + j] 
+                                            + A[(i + 1) * grid_size + j] 
+                                            + A[i * grid_size + j + 1]);
+                error = fmax(error, Anew[i * grid_size + j] - A[i * grid_size + j]); //пересчитываем(узнаём) ошибку
             }
         }
         if (iter % 100 == 0) {// каждую сотую итерацию сохраняем знач-е ошибки на cpu
@@ -82,17 +82,16 @@ int main(int argc, char** argv) {
 #pragma acc wait(1)
         }
 
-        double* temp = kern_1;
-        kern_1 = kern_2;
-        kern_2 = temp;
+        double* temp = A;
+        A = Anew;
+        Anew = temp;
     }
 
     end = clock();
-#pragma acc exit data copyout(kern_1[0:full_size], kern_2[0:full_size])
-#pragma acc update host(kern_1[0:size * size], kern_2[0:size * size], error)
+#pragma acc update host(A[0:size * size], Anew[0:size * size], error)
     std::cout << "Error: " << error << std::endl;
     std::cout << "Iteration: " << iter << std::endl;
-    free(kern_1);
-    free(kern_2);
+    free(A);
+    free(Anew);
     return 0;
 }
